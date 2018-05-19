7d86a6dab51079f5a798984a3a1b771a5c12d71e
hunji <guo194194@gmail.com>
2018-05-20 00:39:29 +0800

    由于svg文件找不到的错误，重新使用了1.1.0版本的代码作为初始前台

---
+++ src/router/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+// 开发环境不使用懒加载, 因为懒加载页面太多的话会造成webpack热更新太慢, 所以只有开发环境使用懒加载
+const _import = require('./import-' + process.env.NODE_ENV)
+
+Vue.use(Router)
+
+// 路由定义使用说明:
+// 1. 代码中路由统一使用name属性跳转.
+// 2. 开放path属性用做简短路由, 比如: '/a1'
+export default new Router({
+  mode: 'hash',
+  routes: [
+    { path: '/404', component: _import('error/404'), name: '404', desc: '404未找到' },
+    { path: '/login', component: _import('login/index'), name: 'login', desc: '登录' },
+    {
+      path: '/',
+      component: _import('layout/index'),
+      name: 'layout',
+      redirect: { name: 'home' },
+      desc: '上左右整体布局',
+      children: [
+        // 通过isTab属性, 设定是否通过tab标签页展示内容
+        { path: '/home', component: _import('home/index'), name: 'home', desc: '首页' },
+        { path: '/layout-setting', component: _import('layout/setting'), name: 'setting', desc: '布局设置' },
+        { path: '/user', component: _import('user/index'), name: 'user', desc: '管理员管理', meta: { isTab: true } },
+        { path: '/role', component: _import('role/index'), name: 'role', desc: '角色管理', meta: { isTab: true } },
+        { path: '/menu', component: _import('menu/index'), name: 'menu', desc: '菜单管理', meta: { isTab: true } },
+        { path: '/sql', component: _import('sql/index'), name: 'sql', desc: 'SQL监控', meta: { isTab: true } },
+        { path: '/schedule', component: _import('schedule/index'), name: 'schedule', desc: '定时任务', meta: { isTab: true } },
+        { path: '/config', component: _import('config/index'), name: 'config', desc: '参数管理', meta: { isTab: true } },
+        { path: '/oss', component: _import('oss/index'), name: 'oss', desc: '文件上传', meta: { isTab: true } },
+        { path: '/log', component: _import('log/index'), name: 'log', desc: '系统日志', meta: { isTab: true } }
+      ],
+      beforeEnter (to, from, next) {
+        let token = Vue.cookie.get('token')
+        if (!token || !/\S/.test(token)) {
+          next({ name: 'login' })
+        }
+        next()
+      }
+    },
+    { path: '*', redirect: { name: '404' } }
+  ]
+})
---
+++ src/views/home/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="mod-home">
+    <h3>项目介绍</h3>
+    <ul>
+      <li>一个轻量级的Java快速开发平台，能快速开发项目并交付【接私活利器】</li>
+      <li>友好的代码结构及注释，便于阅读及二次开发</li>
+      <li>完善的XSS防范及脚本过滤，彻底杜绝XSS攻击</li>
+      <li>实现前后端分离，通过token进行数据交互</li>
+      <li>推荐使用阿里云服务器部署项目，免费领取阿里云优惠券，请点击【<a href="//promotion.aliyun.com/ntms/act/ambassador/sharetouser.html?userCode=y93lfwbg&productCode=dmspre&utm_source=y93lfwbg" target="_blank">免费领取</a>】</li>
+    </ul>
+    <h3>获取帮助</h3>
+    <ul>
+      <li>前端Git地址：<a href="//github.com/daxiongYang/renren-fast-vue" target="_blank">github.com/daxiongYang/renren-fast-vue</a></li>
+      <li>后台Git地址：<a href="//git.oschina.net/renrenio/renren-fast" target="_blank">git.oschina.net/renrenio/renren-fast</a></li>
+      <li>代码生成器：<a href="//git.oschina.net/renrenio/renren-generator" target="_blank">git.oschina.net/renrenio/renren-generator</a></li>
+      <li>如需关注项目最新动态，请Watch、Star项目，同时也是对项目最好的支持</li>
+    </ul>
+    <h3>哈哈</h3>
+    <ul>
+      <li>haha</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style>
+  .mod-home {
+    line-height: 1.5;
+  }
+</style>
+
